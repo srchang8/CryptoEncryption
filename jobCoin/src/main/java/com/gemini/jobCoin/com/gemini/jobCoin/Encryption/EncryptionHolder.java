f14d0b1ee7d530fa6ff967a396c8f69b1825abd9
stephen <srchang8@gmail.com>
2020-03-01 18:50:07 -0500

    add price combination finder

--- jobCoin/src/main/java/com/gemini/jobCoin/com/gemini/jobCoin/Encryption/EncryptionHolder.java
+++ jobCoin/src/main/java/com/gemini/jobCoin/com/gemini/jobCoin/Encryption/EncryptionHolder.java
@@ -397,6 +397,41 @@ public class EncryptionHolder {
         return result;
     }
 
+    static char accountBoard[][] = new char[10][10];
+    static boolean visited[][] = new boolean[10][10];
+
+    public boolean accountExist(char[][] board, String account){
+
+        for (int i=0; i<board.length; i++){
+            for (int j=0; j<board[0].length; j++){
+                if (board[i][j] == account.charAt(0) && accountExistHelper(board, account, i, j, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean accountExistHelper(char[][] board, String account, int row, int col, int count){
+
+        if (row < 0 || row >= board.length || col < 0 || col >= board[row].length ||
+                board[row][col] != account.charAt(count) || visited[row][col]){
+            return false;
+        }
+
+        visited[row][col] = true;
+
+        if (accountExistHelper(board, account, row+1, col, count+1) ||
+                accountExistHelper(board, account, row-1, col, count+1) ||
+                accountExistHelper(board, account, row, col+1, count+1) ||
+                accountExistHelper(board, account, row, col-1, count+1)){
+            return true;
+        }
+
+        visited[row][col] = false;
+        return false;
+    }
+
 }
 
 
