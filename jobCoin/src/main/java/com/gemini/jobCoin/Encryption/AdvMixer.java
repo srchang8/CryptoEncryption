bdc7c286a612d645659662b2803bf88516f432a7
Stephen Chang <stephenchang@Stephens-MacBook-Pro.local>
2021-12-24 12:38:19 -0500

    add min remove

--- jobCoin/src/main/java/com/gemini/jobCoin/Encryption/AdvMixer.java
+++ jobCoin/src/main/java/com/gemini/jobCoin/Encryption/AdvMixer.java
@@ -1,4 +1,39 @@
 package Encryption;
 
+import java.util.Stack;
+
 public class AdvMixer {
+
+    public String minRemove(String s){
+
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> stack = new Stack();
+
+        for (int i=0; i<s.length(); i++){
+
+            char c = s.charAt(i);
+
+            if (c == '('){
+                stack.push(i);
+                continue;
+            }
+
+            if (c == ')'){
+
+                if (!stack.isEmpty()){
+                    stack.pop();
+                }else{
+                    sb.setCharAt(i, '*');
+                }
+            }
+        }
+
+        while (!stack.isEmpty()){
+            sb.setCharAt(stack.pop(), '*');
+        }
+
+        return sb.toString().replaceAll("\\*", "");
+    }
+
+
 }
